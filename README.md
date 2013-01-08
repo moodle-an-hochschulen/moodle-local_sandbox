cfff8e2b1003a8a5a5443151f9cff0e59b08fb24
Alexander Bias <github@alexanderbias.de>
2013-01-08 10:04:32 +0100

    First version of local_sandbox

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
 moodle-local_sandbox
 ====================
+Moodle plugin which programatically restores courses to predefined course states. It can be used to provide playground moodle courses which will be cleaned periodically.
 
-Moodle plugin which automatically restores courses with predefined course backup files on a specific date
+
+Requirements
+============
+This plugin requires Moodle 2.3+
+
+
+Changes
+=======
+2013-01-08 - Initial version
+
+
+Installation
+============
+Install the plugin like any other plugin to folder
+/local/sandbox
+
+See http://docs.moodle.org/23/en/Installing_plugins for details on installing Moodle plugins
+
+
+Usage & Settings
+================
+After installing local_sandbox, the plugin doesn't do anything until it is configured.
+To configure the plugin, please visit Plugins -> Local plugins -> Sandbox.
+
+There, you find three sections:
+
+1. Execution time
+-----------------
+In this section, you define on which days and when on each day local_sandbox should restore the courses.
+
+2. Course backups
+-----------------
+In this section, you define the directory where the course backup files to use for course restoring are. local_sandbox takes every file in this directory with a .mbz filename extension, takes the file's name, searches for a existing course with a shortname equal to the file's name and finally, uses the course backup file to restore / reset this course.
+
+Example:
+The sandbox directory /var/www/files/moodledata/sandbox contains the files foo.bar and mylittlecourse.mbz. local_sandbox looks at the directory and finds two files. File foo.bar is ignored by local_sandbox because it doesn't have the right filename extension. File mylittlecourse.mbz will be considered for restoring a sandbox course. local_sandbox now looks for a existing course with shortname "mylittlecourse". If this course exists, it resets / restores it to the state saved in the mylittlecourse.mbz backup file. If this course doesn't exist, local_sandbox doesn't change anything.
+
+Additionally, in this section, there is an option to set the course start date to today instead of setting it to the date saved in the course backup file. Use this option if you need to provide playground courses in Moodle which pretend to be up-to-date.
+
+3. Notifications
+----------------
+As local_sandbox acts automatically, it can inform you when failures or problems occur. In this section, you can define who should be notified and which failures or problems should be reported.
+
+
+Themes
+======
+The local_sandbox plugin acts behind the scenes, therefore it works with all moodle themes.
+
+
+Further information
+===================
+local_sandbox is found in the Moodle Plugins repository: http://moodle.org/plugins/view.php?plugin=local_sandbox
+
+Report a bug or suggest an improvement: https://github.com/abias/moodle-local_sandbox/issues
+
+
+Copyright
+=========
+Alexander Bias, University of Ulm
---
+++ settings.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__) . '/lib.php');
+
+if ($hassiteconfig) {
+	// New settings page
+	$page = new admin_settingpage('sandbox', get_string('pluginname', 'local_sandbox'));
+
+
+	// Execution time
+	$page->add(new admin_setting_heading('local_sandbox/cronruntimeheading', get_string('cronruntimeheading', 'local_sandbox'), ''));
+
+	// Create days chooser widget
+	$days = array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+	foreach ($days as $d) {
+		$dayschoices[] = get_string($d, 'calendar');
+	}
+	$page->add(new admin_setting_configmulticheckbox2('local_sandbox/cronrunday', get_string('cronrunday', 'local_sandbox'), get_string('cronrundaydescription', 'local_sandbox'), array(), $dayschoices));
+
+	// Create cron run time widget
+	$page->add(new admin_setting_configtime('local_sandbox/cronruntimehour', 'cronruntimemin', get_string('cronruntime', 'local_sandbox'), get_string('cronruntimedescription', 'local_sandbox'), array('h' => 3, 'm' => 0)));
+
+
+	// Course backups
+	$page->add(new admin_setting_heading('local_sandbox/coursebackupsheading', get_string('coursebackupsheading', 'local_sandbox'), ''));
+
+	// Create course backup files directory widget
+	$page->add(new admin_setting_configdirectory('local_sandbox/coursebackupsdirectory', get_string('coursebackupsdirectory', 'local_sandbox'), get_string('coursebackupsdescription', 'local_sandbox'), $CFG->dataroot.'/sandbox'));
+
+	// Create change course start date control widget
+	$page->add(new admin_setting_configcheckbox('local_sandbox/adjustcoursestartdate', get_string('adjustcoursestartdate', 'local_sandbox'), get_string('adjustcoursestartdatedescription', 'local_sandbox'), 0));
+
+
+	// Notifications
+	$page->add(new admin_setting_heading('local_sandbox/notifyheading', get_string('notifyheading', 'local_sandbox'), ''));
+
+	// Create user notification chooser widget
+	$page->add(new admin_setting_users_with_capability('local_sandbox/notifyonerrors', get_string('notifyonerrors', 'local_sandbox'), get_string('notifyonerrorsdescription', 'local_sandbox'), array(), 'moodle/site:config'));
+
+	// Create user notification level widget
+	$levels[LEVEL_NOTICE] = get_string('notice', 'core');
+	$levels[LEVEL_WARNING] = get_string('warning', 'core');
+	$levels[LEVEL_ERROR] = get_string('error', 'core');
+	$page->add(new admin_setting_configselect('local_sandbox/notifylevel', get_string('notifylevel', 'local_sandbox'), get_string('notifyleveldescription', 'local_sandbox'), $levels[LEVEL_ERROR], $levels));
+
+
+	// Add settings page to navigation tree
+    $ADMIN->add('localplugins', $page);
+}
