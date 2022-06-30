5dbffe4b651eb60551b63d1965ae1bffb14f8dd1
Alexander Bias <bias@alexanderbias.de>
2022-06-30 21:35:11 +0200

    Make codechecker happy again

--- classes/restore_controller.php
+++ classes/restore_controller.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * This class inherits basically everything from restore_controller except the fact that it applies the plugin's restore settings.
  *
