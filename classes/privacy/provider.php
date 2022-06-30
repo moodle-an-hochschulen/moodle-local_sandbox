e7d5c815ca329c4ca1636fa34cc50fd36f3909c5
Alexander Bias <bias@alexanderbias.de>
2022-06-30 21:46:33 +0200

    Make codechecker happy again

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,8 +24,6 @@
 
 namespace local_sandbox\privacy;
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Privacy Subsystem implementing null_provider.
  *
--- locallib.php
+++ locallib.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
-
 define('SANDBOX_LEVEL_NOTICE', 1);
 define('SANDBOX_LEVEL_WARNING', 2);
 define('SANDBOX_LEVEL_ERROR', 3);
