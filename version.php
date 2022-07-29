e49d47a77a12696b6275ab6b393eae9aa15e5d37
Alexander Bias <bias@alexanderbias.de>
2022-07-29 22:16:40 +0200

    Release v3.11-r3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_sandbox';
-$plugin->version = 2021102601;
-$plugin->release = 'v3.11-r2';
+$plugin->version = 2021102602;
+$plugin->release = 'v3.11-r3';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
